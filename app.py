235c28acd0567bad5d3531c8a6d348bf8d8ff76f
Padraig Gleeson <p.gleeson@gmail.com>
2026-04-30 19:25:12 +0100

    Mopre testing on app.py

--- app.py
+++ app.py
@@ -1,49 +1,46 @@
+from WormView import WormView
+from WormView import parse_args
+
 import matplotlib.pyplot as plt
 import numpy as np
 import streamlit as st
 import time
-
 
 max_x = 50
 scale = 5
 
 x = np.arange(0, max_x)
 
-# from WormView import get_plot
-# from WormView import parse_args
-
-# args = parse_args()
+args = parse_args()
+args.wcon_file = "/Users/padraig/neuroConstruct/osb/invertebrate/celegans/CE_locomotion/experiments/osc_sim_nml/output.wcon"
+args.wcon_file = "/Users/padraig/neuroConstruct/osb/invertebrate/celegans/WCONViewer/examples/20_R#1_OW956#5_day5_CW_2014_03_03__10_05_43___8___5.wcon"
+args.wcon_file = "examples/simdata.wcon"
 
 print("Loading...")
 
 
-def get_y(t):
-    """Get the y values for the plot based on time t."""
-    return scale * np.sin(
-        2 * np.pi * (x / 50 + t / 5)
-    )  # Example function, can be modified
-
-
 def init():  # give a clean slate to start
-    print("Initializing the plot...")
-    fig, ax = plt.subplots()
-    ax.set_ylim(-1.1 * scale, scale * 1.1)
-    ax.set_xlim(-1, max_x + 1)
+    print(" -- Initializing the plot...")
 
-    (st.session_state.line,) = ax.plot(x, get_y(0), color="red")
-    st.session_state.the_plot = st.pyplot(plt)
+    print("Done initializing.")
 
 
 def animate(t):  # update the y values (every 1000ms)
-    print("Animating the plot for time:", t)
-    plt.close()
-    fig, ax = plt.subplots()
-    ax.set_ylim(-1.1 * scale, scale * 1.1)
-    ax.set_xlim(-1, max_x + 1)
-    (st.session_state.line,) = ax.plot(x, get_y(t), color="green", label=f"t={t:.2f}")
-    plt.legend()
-    # st.session_state.line.set_ydata(get_y(t))
-    st.session_state.the_plot.pyplot(plt)
+    print("\n -- Animating the plot for time:", t)
+
+    st.session_state.the_plot = None
+    plt.close("all")
+
+    wv = WormView()
+    wv.reset()
+    fig, ax = wv.get_plot(args)
+
+    midline_plot, perimeter_plot = wv.update(t)
+
+    st.session_state.line = fig
+    # plt.legend()
+
+    st.session_state.the_plot = st.pyplot(plt)
 
 
 if "t" not in st.session_state:
@@ -52,28 +49,44 @@ if "t" not in st.session_state:
     st.session_state.t = 0
     animate(0)  # initial animation to set the plot
 
+if "running" not in st.session_state:
+    st.session_state.running = False
 
-if st.button("Step"):
-    print("Stepping...")
-    st.session_state.t += 0.1
+
+st.markdown(f"Time {st.session_state.t}, running {st.session_state.running}.")
+
+
+def step(n):
+    print(f" - Stepping forward {n} steps...")
+    st.session_state.t += n
     animate(st.session_state.t)
 
-if st.button("Stop"):
-    print("Stopping...")
-    st.session_state.t += 0.1
-    animate(st.session_state.t)
-
-play = False
 
 if st.button("Play"):
     play = True
-    print("Playing...")
-    for i in range(50):
-        if not play:
+
+    st.session_state.running = True
+
+    for i in range(5):
+        if not st.session_state.running:
             break
-        animate(st.session_state.t)
-        st.session_state.t += 0.1
+        step(1)
         time.sleep(0.1)
+        st.rerun()
 
+if st.session_state.running:
+    print("(re)running")
 
-st.markdown(f"Time {st.session_state.t}.")
+    step(1)
+    time.sleep(0.1)
+    st.rerun()
+
+if st.button("Step"):
+    step(1)
+
+if st.button("FFWD"):
+    step(5)
+
+if st.button("Stop"):
+    print("Stopping...")
+    st.session_state.running = False
