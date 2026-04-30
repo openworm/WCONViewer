733f6eef5703c601959d62eb9d68d615f71d337b
Padraig Gleeson <p.gleeson@gmail.com>
2026-04-30 09:13:55 +0100

    Add more example wcons

--- test_all.sh
+++ test_all.sh
@@ -5,3 +5,5 @@ ruff format *.py
 ruff check *.py
 
 python WormView.py -f examples/simdata.wcon -nogui # Test reloading WCON in Player
+python WormView.py -f examples/worm_motion_log.wcon -nogui
+python WormView.py -f examples/output_W2D.wcon -nogui
